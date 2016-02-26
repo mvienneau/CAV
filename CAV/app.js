be547d66d18ff2dac8f32d6a216629a506bc4d95
Tyler Scott <tylersco@rgnt1-48-235-dhcp.int.colorado.edu>
2016-02-26 13:41:49 -0700

    Add search and account jade pages, linked with app.js and index.js route

--- CAV/app.js
+++ CAV/app.js
@@ -23,6 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/search', routes);
+app.use('/account', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
