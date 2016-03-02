42dca6ad34b4e39bd6b1d9ec34300227b23ea6ff
MICHAEL VIENNEAU <michael.vienneau@colorado.edu>
2016-03-02 16:29:26 -0700

    added login functionality, does not check for PW

--- CAV/public/javascripts/global.js
+++ CAV/public/javascripts/global.js
@@ -1,8 +1,52 @@
 $(document).ready(function() {
-
+	//alert("YO");
     $('#btnSubmit').on('click', addUser);
+    $('#loginSubmit').on('click', userLogin);
 
 });
+
+function userLogin(event){
+	//alert("LOGGING IN");
+	event.preventDefault();
+	var errorCount = 0;
+	$('#login input').each(function(index, val) {
+		if ($("#login fieldset input#username").val() ===  '' || $("#login fieldset input#password").val() === ''){
+			errorCount++;
+		}
+	});
+	if (errorCount === 0){
+		var user = {
+			'username': $('#login fieldset input#username').val(),
+			'password': $('#login fieldset input#password').val()
+		}
+		$.ajax({
+			type: 'POST',
+			data: user,
+			url: '/login',
+			datatype: 'json',
+			success: function(response){
+				if (response.success == true) {
+					alert('Sucessfully Logged In');
+					window.location.href = "/"
+				}
+				else {
+					console.log(response);
+					alert('Cannot Login, Username or Password is incorrect');
+					window.location.href = "/login"
+				}
+			},
+			error: function(response){
+				alert('Error: ' + response.msg);
+				window.location.href = "/login"	
+			}
+		});
+	}
+	else{
+		alert('please fill in all fields');
+		return false;
+	}
+}
+
 
 function addUser(event){
 	event.preventDefault();
@@ -47,4 +91,5 @@ function addUser(event){
 		alert('Please fill in required fields');
 		return false;
 	}
-};
+}
+
--- CAV/routes/login.js
+++ CAV/routes/login.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.post('/', function(req, res){
+  var db = req.db;
+  var exists = true;
+  db.serialize( function() {
+    db.all("select count(*) as count from user where username = \"" + req.body.username + "\"", function(err, row) {
+      if (row[0].count == 0){
+        exists = false;
+        res.json({success: false, error: true});
+        res.end();
+      }
+      if (exists === true){
+        res.json({success: true, error: false});
+        req.session.username = req.body.username;
+        req.session.save();
+        res.end();
+      }
+    });
+    /*
+    db.all("select count(*) as count from user where password = \"" + req.body.password + "\"", function(err, row) {
+      if (row[0].count == 0){
+        exists = false;
+        res.json({success: false, error: true});
+        res.end();
+      }
+    });
+*/
+  });
+
+  
+})
+
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   var sess = req.session;
